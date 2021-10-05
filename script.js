5f9663583247077d613a5e331afe28e002c7d719
reesericci <reesericci@nyctech.cloud>
2021-10-05 11:26:56 -0500

    disable quickhide works now

--- script.js
+++ script.js
@@ -1,11 +1,13 @@
 let quickHideEnabled = true;
 document.addEventListener('keydown', function(event) {
-event.preventDefault();
 console.log(event)
 if((event.key == "q" || event.key == "Q") && event.altKey) {
+  event.preventDefault();
   quickHideEnabled = !quickHideEnabled
 } else if ((event.key == "q" || event.key == "Q") && quickHideEnabled == true) {
   document.write()
   window.location = "https://aisdblend.instructure.com"
 }
 })
+
+
