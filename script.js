306522c28acab94ff5373fab5b2f636de8a170f0
reesericci <reesericci@nyctech.cloud>
2021-10-05 10:38:55 -0500

    Revert "Revert "disabling quick hide""
    This reverts commit c04a0c9add8833120c018eedba8dfe5fae6c4102.

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
-function quickHide(e) {
-if(event.key == "q" || event.key == "Q") {
-document.write()
-window.location = "https://aisdblend.instructure.com"
+let quickHideEnabled = true;
+document.addEventListener('keydown', function(event) {
+event.preventDefault();
+console.log(event)
+if((event.key == "q" || event.key == "Q") && event.altKey) {
+  quickHideEnabled = !quickHideEnabled
+} else if ((event.key == "q" || event.key == "Q") && quickHideEnabled == true) {
+  document.write()
+  window.location = "https://aisdblend.instructure.com"
 }
-} 
+})
