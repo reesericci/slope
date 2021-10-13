dfb9c3bc8341fc262825bc9935e547830a6b0083
willnotbowman <92473898+willnotbowman@users.noreply.github.com>
2021-10-13 15:12:52 -0500

    Update script.js

--- script.js
+++ script.js
@@ -4,13 +4,7 @@ console.log(event)
 if((event.key == "q" || event.key == "Q") && event.altKey) {
   event.preventDefault();
   quickHideEnabled = !quickHideEnabled
-} else if ((event.key == "q" || event.key == "Q") && quickHideEnabled == true) {
-  document.write()
-  window.location = "https://aisdblend.instructure.com"
-} else if ((event.key == "ctrl") && quickHideEnabled == true) {
-  document.write()
-  window.location = "https://aisdblend.instructure.com"
-} else if ((event.key == "shift") && quickHideEnabled == true) {
+} else if ((event.key == "q" || event.key == "Q" || event.key == 'ctrlKey' || event.key == 'shiftKey') && quickHideEnabled == true) {
   document.write()
   window.location = "https://aisdblend.instructure.com"
 }
